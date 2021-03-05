7c8b20bd7e9bb4930c5a56ae3d36de0193b135b9
R-Technology <43551882+Farizal97@users.noreply.github.com>
2021-03-05 11:05:15 +0700

    penggajian codeigntier 3

---
+++ application/controllers/admin/potonganGaji.php
@@ -0,0 +1,16 @@
+<?php 
+
+class potonganGaji extends CI_Controller
+{
+    public function index ()
+    {
+        $data['title'] = "Setting Potongan Gaji";
+        $data['pot_gaji'] = $this->penggajianModel->get_data('potongan_gaji')->result();
+        $this->load->view('templates_admin/header', $data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/potonganGaji', $data);
+        $this->load->view('templates_admin/footer');
+    }
+}
+
+?>
